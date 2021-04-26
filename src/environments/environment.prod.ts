d89cf6db1f7cb8c0e3eb7954205962238efd6b08
rivierha <richagaur507@gmail.com>
2021-04-26 17:18:19 +0530

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA6qiAF0T8e5FHyiUHZEnGNLJNkVFyBPiQ",
+    authDomain: "staging-stack-management-app.firebaseapp.com",
+    projectId: "staging-stack-management-app",
+    storageBucket: "staging-stack-management-app.appspot.com",
+    messagingSenderId: "191605711614",
+    appId: "1:191605711614:web:4e0bfc7acefc098f2fbbaf"
+  },
+  api: "https://ec2-54-82-22-93.compute-1.amazonaws.com:3000/api/v1/"
 };
